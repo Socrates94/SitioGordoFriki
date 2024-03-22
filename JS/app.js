bc4bf7db4f49a110b2fe52f0ada09a3f958a6421
Daniel Flores <daniel_f94@outlook.com>
2024-03-21 22:21:19 -0600

    new file:   JS/app.js new file:   clientes.html new file:   contactos.html new file:   img/banner1.jpeg new file:   img/banner2.jpeg new file:   img/banner3.jpeg new file:   img/banner4.jpeg new file:   img/banner5.jpeg new file:   img/banner6.jpeg new file:   img/logo.ico new file:   index.html new file:   nosotros.html new file:   style/normalize.css new file:   style/style.css

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+
+bars = document.querySelector(".bars");
+bars.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+window.onload = function(){
+    // alert('Homla');
+    $('#spin').fadeOut();
+    $('body').removeClass('hidden');
+}
